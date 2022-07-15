591008e998a7803771f2b0418f607d709211577c
Asmik <hasohovsepyan@bk.ru>
2022-07-15 13:18:01 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int count = 100;
-          int addCount = 1100;
-          int bonus = 0;
-         if (addCount > 0) {
+        int addCount = 1100;
+        int bonus = 0;
+        if (addCount > 0) {
             if (addCount > 1000) {
                 bonus = addCount / 100;
             }
             count = count + bonus + addCount;
             System.out.println("На вашем счёте: " + count + " рублей. " + "Ваш бонус: " + bonus + " рублей.");
-         } else {
+        } else {
             System.out.println("Сумма пополнения не может быть отрицательной или равна нулю!");
         }
     }
