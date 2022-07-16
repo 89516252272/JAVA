f287fe24520d7666f2bb7a9b6ba75161695b30ea
Asmik <hasohovsepyan@bk.ru>
2022-07-16 13:17:53 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int count = 100;
-        int addCount = 1100;
-        int bonus = 0;
-        if (addCount > 0) {
-            if (addCount > 1000) {
-                bonus = addCount / 100;
-            }
-            count = count + bonus + addCount;
-            System.out.println("На вашем счёте: " + count + " рублей. " + "Ваш бонус: " + bonus + " рублей.");
-        } else {
-            System.out.println("Сумма пополнения не может быть отрицательной или равна нулю!");
-        }
+
+        int ticketPrice = 15000;
+        int miles = ticketPrice / 20;
+        System.out.println("Вам начислят " + miles + " миль.");
+
     }
+
 }
